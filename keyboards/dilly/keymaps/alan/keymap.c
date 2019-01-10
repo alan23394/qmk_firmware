16e2f4949819c2524e1bfd88889c0ce812561919
Alan Barnett <alanbarnett328@gmail.com>
2019-01-09 19:27:19 -0800

    Layout changes

--- keyboards/dilly/keymaps/alan/keymap.c
+++ keyboards/dilly/keymaps/alan/keymap.c
@@ -14,15 +14,17 @@ extern keymap_config_t keymap_config;
 // Tap-Hold keys
 #define KC_ASFT MT(MOD_LSFT, KC_A)
 #define KC_T_L3 LT(_FN3, KC_T)
-#define KC_ZCTL MT(MOD_LCTL, KC_Z)
+//#define KC_ZCTL MT(MOD_LCTL, KC_Z)
+#define KC_ZSFT MT(MOD_LSFT, KC_Z)
 #define KC_XALT MT(MOD_LALT, KC_X)
 #define KC_CGUI MT(MOD_LGUI, KC_C)
 #define KC_D_L4 LT(_FN4, KC_D)
-#define KC_SPL2 LT(_FN2, KC_SPC)
+#define KC_SPL1 LT(_FN1, KC_SPC)
 #define KC_B_L1 LT(_FN1, KC_B)
 #define KC_H_L5 LT(_FN5, KC_H)
 #define KC_KALT MT(MOD_RALT, KC_K)
-#define KC_ESCT MT(MOD_RCTL, KC_ESC)
+//#define KC_ESCT MT(MOD_RCTL, KC_ESC)
+#define KC_ESL2 LT(_FN2, KC_ESC)
 #define KC_ENTS MT(MOD_RSFT, KC_ENT)
 // #define KC_ESCS MT(MOD_RSFT, KC_ESC)
 
@@ -30,17 +32,43 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+	/*
   [_BASE] = LAYOUT_ortho_3x10(
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_V,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_BSPC,
     KC_ASFT, KC_R,    KC_S,    KC_T_L3, KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
     KC_ZCTL, KC_XALT, KC_CGUI, KC_D_L4, KC_SPL2, KC_B_L1, KC_H_L5, KC_KALT, KC_ESCT, KC_ENTS
   ),
+  */
+  [_BASE] = LAYOUT_ortho_3x10(
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_V,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_BSPC,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
+    KC_ZSFT, KC_X,    KC_C,    KC_D_L4, KC_SPL1, KC_ESL2, KC_H_L5, KC_B,    KC_K,    KC_ENTS
+  ),
 
+  /*
   [_FN1] = LAYOUT_ortho_3x10(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     _______, _______, _______, _______, KC_BSPC, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
   ),
+  */
+  [_FN1] = LAYOUT_ortho_3x10(
+    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TILD, KC_SLSH, KC_7,    KC_8,    KC_9,    KC_MINS,
+    KC_LABK, KC_RABK, KC_LCBR, KC_RCBR, KC_EQL,  KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS,
+    _______, _______, _______, _______, _______, KC_BSPC, KC_1,    KC_2,    KC_3,    KC_0
+  ),
+
+  /*
+  ** Esc  `    Ins  Home Pgup           !    ?    \
+  ** Tab       Del  End  Pgdn      .    ,    ;    '
+  ** Shft Ctrl Alt  Win        ()
+  **      +--OneShot--+
+  */
+  /*
+  ** [    ]    (    )    ~    /    7    8    9    -
+  ** <    >    {    }    =    *    4    5    6    +
+  **                      ()  Bksp 1    2    3    0
+  */
 
   [_FN2] = LAYOUT_ortho_3x10(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
